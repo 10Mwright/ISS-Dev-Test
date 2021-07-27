1658dcfd0629b86f6a9502c5a97b9945ae74e773
10MWright <Thewrighty@gmail.com>
2021-07-27 12:48:42 +0100

    fixed global var not being read properly

--- Script/barchart.js
+++ Script/barchart.js
@@ -1,59 +1,62 @@
-picasso.chart({
-    element: document.getElementById('charting').querySelector('#barchart'),
-    data: [
-        {
-            type: 'bar',
-            data: [
-                ['Month', 'Sales'],
-                ['Jan', 1000],
-                ['Feb', 2000],
-                ['Mar', 4000],
-            ],
-        },
-    ],
-    settings: {
-        scales: {
-            y: {
-                data: {field: 'Sales'},
-                invert: true,
-                include: [0]
-            },
-            c: {
-                data: {field: 'Sales'},
-                type: 'color'
-            },
-            t: {data: { extract: {field: 'Month'}}, padding: 0.3},
-        },
-        components: [
+setTimeout(function () { //Delay to avoid undefined variable
+    for(var i = 0; i < chartData[i].length; i++) {
+        for(var z = 0; z < chartData.length; z++) {
+          console.log(chartData[z][i]);
+        }
+      }
+
+    picasso.chart({
+        element: document.getElementById('charting').querySelector('#barchart'),
+        data: [
             {
-                type: 'axis',
-                scale: 'y',
-                dock: 'left'
+                type: 'bar',
+                data: chartData,
             },
-            {
-                type: 'axis',
-                scale: 't',
-                dock: 'bottom'
+        ],
+        settings: {
+            scales: {
+                y: {
+                    data: { field: 'Sales' },
+                    invert: true,
+                    include: [0]
+                },
+                c: {
+                    data: { field: 'Sales' },
+                    type: 'color'
+                },
+                t: { data: { extract: { field: 'IFA' } }, padding: 0.3 },
             },
-            {
-                key: 'bars',
-                type: 'box',
-                data: {
-                    extract: {
-                        field: 'Month',
-                        props: {
-                            start: 0,
-                            end: {field: 'Sales'}
+            components: [
+                {
+                    type: 'axis',
+                    scale: 'y',
+                    dock: 'left'
+                },
+                {
+                    type: 'axis',
+                    scale: 't',
+                    dock: 'bottom'
+                },
+                {
+                    key: 'bars',
+                    type: 'box',
+                    data: {
+                        extract: {
+                            field: 'IFA',
+                            props: {
+                                start: 0,
+                                end: { field: 'Sales' }
+                            }
+                        }
+                    },
+                    settings: {
+                        major: { scale: 't' },
+                        minor: { scale: 'y' },
+                        box: {
+                            fill: { scale: 'c', ref: 'end' }
                         }
                     }
-                },
-                settings: {
-                    major: {scale: 't'},
-                    minor: {scale: 'y'},
-                    box: {
-                        fill: {scale: 'c', ref:'end'}
-                    }
-                }
-            }]
-    }
-});
+                }]
+        }
+    });
+}, 2000);
--- Script/data_import.js
+++ Script/data_import.js
@@ -5,66 +5,67 @@
         - Work on calculating totals for each ifa
 */
 
-var data = [];
+window.chartData = [];
 
-$(document).ready(function(){
-    $.getJSON("Resources/dev_test.dat", data, function(data){
-        var dataStore = [];
+$.getJSON("Resources/dev_test.dat", data, function (data) {
+    var uniqueIFA = findIFAS(data);
 
-        var uniqueIFA = findIFAS(data);
+    console.log("Array of unique IFAs: " + uniqueIFA);
 
-        console.log(uniqueIFA);
+    //Now need to run through each IFA, totalling up the sales for each
 
-        //Now need to run through each IFA, totalling up the sales for each
+    //For each unique IFA, go through the data and total up its sales
+    for (var i = 1; i < uniqueIFA.length; i++) {
+        var total = 0;
 
-        for(var i = 0; i < uniqueIFA.length; i++) {
-            var total = 0;
+        var currentIFAData = data.filter(function (obj) {
+            return obj.ifa === uniqueIFA[i][0];
+        });
 
-            var currentIFAData = data.filter(function(obj) {
-                return obj.ifa === uniqueIFA[i][0];
-            });
-
-            //For each row for this IFA add to IFA's total
-            for(var j = 0; j < currentIFAData.length; j++) {
-                total += parseInt(currentIFAData[j].sales);
-            }
-
-            console.log("IFA TOTAL FOR " + uniqueIFA[i][0] + ": " + total);
+        //For each row of data for this IFA add to IFA's total sales
+        for (var j = 0; j < currentIFAData.length; j++) {
+            total += parseInt(currentIFAData[j].sales);
         }
 
-        /*
-        $.each(data, function() {
-            var total = 0;
+        console.log("IFA TOTAL FOR " + uniqueIFA[i][0] + ": " + total);
+        uniqueIFA[i][1] = total; //Commit sales total to 2d IFA array
+    }
 
-            var currentIFAData = data.filter(function(obj) {
-                return obj.ifa = uniqueIFA[2][2];
-            });
+    console.log("Array of unique IFAs: " + uniqueIFA);
+    document.getElementById('output').innerHTML = uniqueIFA;
+    chartData = uniqueIFA.slice();
 
-            console.log(uniqueIFA[2][0]);
-            console.log("IFA Data:" + currentIFAData);
+    /*
+    $.each(data, function() {
+        var total = 0;
 
-            for(var i = 0; i < currentIFAData.length; i++) {
-                total += parseInt(currentIFAData[i].sales);
-            }
-            console.log("total:" + total);
-        })*/
+        var currentIFAData = data.filter(function(obj) {
+            return obj.ifa = uniqueIFA[2][2];
+        });
 
-    })
-    $.getJSON("Resources/dev_test.dat", function(data){
-        console.log(data);
-    }).fail(function() {
-        console.log("error in JSON file parsing");
-    });
+        console.log(uniqueIFA[2][0]);
+        console.log("IFA Data:" + currentIFAData);
+
+        for(var i = 0; i < currentIFAData.length; i++) {
+            total += parseInt(currentIFAData[i].sales);
+        }
+        console.log("total:" + total);
+    })*/
+
+}).fail(function () {
+    console.log("error in retrieving data from JSON file!");
 });
 
 //Function to return an array of unique IFAs mentioned in the data
 //Source used: https://stackoverflow.com/questions/7431618/jquery-finding-distinct-values-in-object-array
 function findIFAS(data) {
-    var uniqueIFA = [];
     var duplicateIFA = {};
+    var uniqueIFA = [['IFA', 'Sales']];
 
-    $.each(data, function(i, el) {
-        if(!duplicateIFA[el.ifa]) {
+    console.log("RAN!");
+
+    $.each(data, function (i, el) {
+        if (!duplicateIFA[el.ifa]) {
             duplicateIFA[el.ifa] = true;
             uniqueIFA.push([el.ifa, 0]);
         }
