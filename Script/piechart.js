816e0e2d660de441865083b0a9424f1f8b5df3fc
10MWright <Thewrighty@gmail.com>
2021-07-29 23:44:26 +0100

    fixed issue with getData method in piechart

--- Script/piechart.js
+++ Script/piechart.js
@@ -4,13 +4,14 @@ getData();
 
 //Gather relevant data
 function getData() {
+    console.log("test!");
     $.getJSON("Resources/dev_test.dat", data, function(data) {
         //First get unique ifas, and get data on a per ifa basis
         //Must calculate market share by dividing ifa's total sales by overall total
         //Then work on filtering out years and funds
         var uniqueIFA = findIFAS(data);
 
-        for(var i = 0; i < uniqueIFA; i++) { //For each unique IFA
+        for(var i = 0; i < uniqueIFA.length; i++) { //For each unique IFA
             var total = 0;
 
             var currentIFAData = data.filter(function(obj) { //Get IFAs data
@@ -21,7 +22,7 @@ function getData() {
                 total += parseInt(currentIFAData[j].sales);
             }
 
-            console.log("Sales total for: " + uniqueIFA[j] + " is " + total);
+            console.log("Sales total for: " + uniqueIFA[i] + " is " + total);
 
             //Must convert original 1D array into 2D array to store this value.
         }
